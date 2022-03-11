35371a5a3f519cf6973362b6d093b00085f62853
Gizmotronn <droidology@allianceofdroids.org.au>
2022-03-11 10:38:21 +0800

    #1 -> Add Form functions (currently empty) for mapping metamask input for transactions

--- webapp/client/src/components/Navbar.jsx
+++ webapp/client/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
+import React from 'react';
+
 import logo from '../../images/logo.png';
 
 const NavbarItem = ({ title, classProps }) => {
@@ -20,7 +22,7 @@ const Navbar = () => {
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
                 <img src={logo} alt="logo" className="w-32 cursor-pointer" />
             </div>
-            <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
                 {["Market ", "Exchange ", "Tutorials ", "Wallets "].map((item, index) => (
                     <NavbarItem key={item + index} title={item} />
                 ))}
@@ -30,9 +32,20 @@ const Navbar = () => {
             </ul>
             <div className="flex relative">
                 {toggleMenu
-                    ? <AiOutlineClose />
-                    : <HiMenuAlt4 />
+                    ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
+                    : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
                 }
+                {toggleMenu && ( // Only if mobile navigation bar is opened
+                    <ul className='z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'>
+                        <li className="text-xl w-full my-2">
+                            <AiOutlineClose onClick={() => setToggleMenu(false)} />
+                        </li>
+                        {["Market ", "Exchange ", "Tutorials ", "Wallets "].map((item, index) => (
+                            <NavbarItem key={item + index} title={item} classProps="my-2 text-lg" />
+                        ))}
+                    </ul>
+                )}
             </div>
         </nav>
     )
