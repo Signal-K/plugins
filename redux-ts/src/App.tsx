4439faf048832f0972337288477622872d77d253
Gizmotronn <droidology@allianceofdroids.org.au>
2022-03-24 15:06:44 +0800

    Set up Redux search tool with axios for #1

--- redux-ts/src/App.tsx
+++ redux-ts/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import { store } from './state/store'
+import RepositoriesList from './components/RepositoriesList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return <Provider store={store}>
+        <div>
+            <h1>Search for a node package</h1>
+            <RepositoriesList />
+        </div>
+    </Provider>
+};
 
 export default App;
