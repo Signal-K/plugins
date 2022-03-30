cb7c1b41e6e0f3a514685f0feff706b0fd993335
Gizmotronn <droidology@allianceofdroids.org.au>
2022-03-30 10:33:23 +0800

    Dynamic fetching & loading of node modules for #1

--- docs/src/plugins/unpkg-path-plugin.ts
+++ docs/src/plugins/unpkg-path-plugin.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
 
 export const unpkgPathPlugin = () => {
@@ -6,7 +7,23 @@ export const unpkgPathPlugin = () => {
         setup(build: esbuild.PluginBuild) {
             build.onResolve({ filter: /.*/ }, async (args: any) => {
                 console.log('onResole', args); // onResolve - figure out where the file is
-                return { path: args.path, namespace: 'a' };
+                if (args.path === 'index.js') {
+                    return { path: args.path, namespace: 'a' };
+                }
+
+                // URL joiner - to navigate to the correct file based on path in the desired module. Joining the url between unpkg and the path
+                if (args.path.includes('./' || args.path.includes('../'))) {
+                    return {
+                        namespace: 'a',
+                        //path: new URL(args.path, args.importer + '/').href, // Connect the path of the module to import to the file created by the end user (which is being imported -> importer)
+                        path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
+                    }
+                }
+
+                return {
+                    namespace: 'a',
+                    path: `https://unpkg.com/${args.path}`,
+                }
             });
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,10 +33,20 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: `
-                import message from 'tiny-test-pkg';
-                console.log(message);
+                import React, { useState } from 'react';
+                const reactDOM = require('react-dom');
+                console.log(react, useState, reactDOM);
                 `,
                     };
+                }
+
+                const { data, request } = await axios.get(args.path); // data now contains the text content of the index.js file of the desired package
+                console.log(data);
+                console.log(request);
+                return {
+                    loader: 'jsx', // apply jsx loader - parse jsx inside the file retrieved
+                    contents: data,
+                    resolveDir: new URL('./', request.responseURL).pathname, // resolveDir - the directory of the file retrieved
                 }
             });
         },
