bf2a98f5541a02219f7d8bdedba66ab4214dc0a8
Gizmotronn <droidology@allianceofdroids.org.au>
2022-03-30 10:54:09 +0800

    Add localforage DB to #1

--- docs/src/plugins/unpkg-path-plugin.ts
+++ docs/src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,20 @@
 import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+    name: 'filecache', // name of the db
+});
+
+/*
+Testing an immediately invoked function to invoke indexedDB
+(async () => {
+    await fileCache.setItem('color', 'red');
+
+    // Retrieve the key
+    const color = await fileCache.getItem('color');
+    console.log(color);
+})()*/
 
 export const unpkgPathPlugin = () => {
     return {
@@ -40,14 +55,26 @@ export const unpkgPathPlugin = () => {
                     };
                 }
 
+                // Check to see if the module/file has already been fetched and if it is in the cache
+                // Return file immediately if it is in the cache
+                const cachedResult = await fileCache.getItem(args.path); // If result is null, it hasn't been fetched and then put in the cache
+                if (cachedResult) {
+                    return cachedResult;
+                }
+                // If file is not in the cache
                 const { data, request } = await axios.get(args.path); // data now contains the text content of the index.js file of the desired package
+                // Return object to console
                 console.log(data);
                 console.log(request);
-                return {
+                // Store response in cache -> `args.path` is the key
+                const result = {
                     loader: 'jsx', // apply jsx loader - parse jsx inside the file retrieved
                     contents: data,
                     resolveDir: new URL('./', request.responseURL).pathname, // resolveDir - the directory of the file retrieved
                 }
+                // store response in cache 2
+                await fileCache.setItem(args.path, result);
+                return result;
             });
         },
     };
