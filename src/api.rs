70d2cbe2784c2440fe250775ddd42e7b863c2eba
Liam Arbuckle <liam@skinetics.tech>
2023-01-22 21:12:26 +1100

    💍🍅 ↝ Add frontend submodule, updating ruby API to be accessible to React

--- src/api.rs
+++ src/api.rs
@@ -2,5 +2,15 @@ use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/api/plugins")]
 async fn get_plugins() -> impl Responder {
-    let plugins = vec!["plugin"]
+    let plugins = vec!["plugin_a", "plugin_b", "plugin_c"]; // Replace with your actual plugin information
+
+    HttpResponse::Ok().json(plugins)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_plugins))
+        .bind("127.0.0.1:8080")? // Replace with your desired address and port
+        .run()
+        .await
 }
