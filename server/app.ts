f6b280c5ea89f96bd6877e7e43faf47a08a7ac0d
Liam Arbuckle <liam@skinetics.tech>
2023-07-05 13:18:56 +1000

    🌟🪇 ↝ Add listener in Express/ts

--- server/app.ts
+++ server/app.ts
@@ -1,1 +1,20 @@
 import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/read', (req: Request, res: Response) => {
+    res.send('This is the first textual component fetched from the Node frontend');
+});
+
+app.post('/send', (req: Request, res: Response) => {
+    const text = req.body.text;
+    res.send('Cool');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
